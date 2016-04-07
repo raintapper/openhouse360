fec0f31633d71ce3d54a3da82b5d8be8ffa86a3d
raintapper <chew.barry@gmail.com>
2016-04-07 13:28:47 +0000

    Updated Packages List and Page

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create!(name:		              "Example	User",
-             email:	                "example@railstutorial.org",
+User.create!(name:		              "openhouse360",
+             email:	                "openhouse360.info@gmail.com",
              password:							"foobar",
              password_confirmation:	"foobar",
              admin: true,
